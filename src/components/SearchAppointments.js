f6cba4e1bca1abbc5ba3823ae363ae84857eaa6e
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-23 12:35:06 -0500

    add components for appointments add, search and list

---
+++ src/components/SearchAppointments.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchAppointments extends Component {
+  render() {
+    return (
+      <div>
+        Search Appointments
+      </div>
+    )
+  }
+}
