1879c6f3e82e027080c1b0b70827fce39c899765
Edwin Dominguez <edwindominguez@hotmail.com>
2019-11-25 22:45:48 -0500

    implement modify content functionality

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -17,12 +17,19 @@ export default class ListAppointments extends Component {
         {this.props.appointments.map(item => (
           <div className="pet-item col media py-3" key={item.aptId}>
             <div className="mr-3">
-              <button className="pet-delete btn btn-sm btn-danger" onClick={()=> this.props.deleteAppointment(item)}><FaTimes /></button>
+              <button className="pet-delete btn btn-sm btn-danger" onClick={() => this.props.deleteAppointment(item)}><FaTimes /></button>
             </div>
 
             <div className="pet-info media-body">
               <div className="pet-head d-flex">
-                <span className="pet-name">{item.petName}</span>
+                <span
+                  className="pet-name"
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={
+                    e => this.props.updateInfo('petName', e.target.innerText, item.aptId)
+                  }
+                >{item.petName}</span>
                 <span className="apt-date ml-auto">
                   <Moment
                     date={item.aptDate}
@@ -31,8 +38,22 @@ export default class ListAppointments extends Component {
                 </span>
               </div>
 
-              <div className="owner-name">
-                <span className="label-item">Owner: </span>
+              <div
+                className="owner-name"
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={
+                  e => this.props.updateInfo('ownerName', e.target.innerText, item.aptId)
+                }
+              >
+                <span
+                  className="label-item"
+                  contentEditable
+                  suppressContentEditableWarning
+                  onBlur={
+                    e => this.props.updateInfo('aptNotes', e.target.innerText, item.aptId)
+                  }
+                >Owner: </span>
                 <span>{item.ownerName}</span>
               </div>
               <div className="apt-notes">{item.aptNotes}</div>
